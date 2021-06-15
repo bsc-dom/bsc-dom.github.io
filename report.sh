abf33f0b0f0d96adff7875fe2872d8f38db0db28
dgasull <dgasull@bsc.es>
2021-06-15 11:52:34 +0200

    Modified report.sh script to use dom-ci infrastructure

---
+++ report.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# get test results
+echo " ** Getting results ** "
+mkdir -p $HOME/all-allure-results
+
+for APPVEYOR_JOB_RESULTS in $HOME/allure-results/* ; do
+    cp -r $APPVEYOR_JOB_RESULTS/* $HOME/all-allure-results/
+done
+echo " ** Obtained results ** "
+
+
+echo " ** Getting history ** "
+HISTORY_DIR="testing-report/history"
+if [ -d "$HISTORY_DIR" ]; then
+	cp -R $HISTORY_DIR /tmp/allure-results/history
+fi
+echo " ** Obtained history ** "
+
+
+echo " ** Generating executor ** "
+# generate executor
+EXECUTOR='{
+			"name":"Appveyor",
+		 	"type":"appveyor",
+			"url": "https://ci.appveyor.com/",
+			"buildUrl": "https://ci.appveyor.com/"
+}'
+echo "$EXECUTOR" > /tmp/allure-results/executor.json
+echo " ** Generated executor ** "
+
+# remove previous report
+echo " ** Removing previous report ** "
+git rm -rf testing-report/*
+echo " ** Removed previous report ** "
+	
+# generate report
+echo " ** Generating report ** "
+$HOME/allure/bin/allure generate $HOME/all-allure-results/ -o testing-report --clean
+echo " ** Generated report ** "
+
+# publish
+echo " ** Publishing ** "
+sed -i -e "s~base href=\"/\"~base href=\"/testing-report/\"~g" testing-report/index.html
+git add -A
+git commit -m "Updating test report from Appveyor"
+git push origin HEAD:master
+echo " ** Published ** "
+
+# remove last test results
+echo " ** Removing results ** "
+rm -rf $HOME/all-allure-results
+rm -rf $HOME/allure-results/*
+echo " ** Removed results ** "
